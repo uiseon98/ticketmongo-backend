fff00d6ded227531d06a309ee3cea185a7cb4120
hsu-git <hsu080206@gmail.com>
2025-06-16 04:27:27 +0900

    feat: 초기 세팅 폴더 구조 및 .gitkeep 추가

---
+++ src/main/java/com/team03/ticketmon/TicketmonGoApplication.java
@@ -0,0 +1,13 @@
+package com.team03.ticketmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmonGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketmonGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team03/ticketmon/TicketmonGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team03.ticketmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmonGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
