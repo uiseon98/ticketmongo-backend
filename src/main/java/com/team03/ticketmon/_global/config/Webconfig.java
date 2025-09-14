df135ee995abacdfba94c1c772cd0ddcb0200c65
uiseon98 <3dnjf14dlfzz@gmail.com>
2025-09-14 18:08:03 +0900

    webconfig 수정

--- src/main/java/com/team03/ticketmon/_global/config/Webconfig.java
+++ src/main/java/com/team03/ticketmon/_global/config/Webconfig.java
@@ -2,10 +2,14 @@ package com.team03.ticketmon._global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
+public class WebConfig implements WebMvcConfigurer {
 
-public class Webconfig {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
@@ -14,9 +18,11 @@ public class Webconfig {
                 .addResolver(new PathResourceResolver() {
                     @Override
                     protected Resource getResource(String resourcePath, Resource location) {
+                        // API 경로 요청은 이 로직에서 제외합니다.
                         if (resourcePath.startsWith("api/")) {
                             return null;
                         }
+
                         Resource requestedResource = location.createRelative(resourcePath);
                         return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
                                 : new ClassPathResource("/static/index.html");
