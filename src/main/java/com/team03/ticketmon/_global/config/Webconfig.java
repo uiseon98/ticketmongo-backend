20bca3018b02c0bf7e3621e2da0890f4008b8b8b
uiseon98 <3dnjf14dlfzz@gmail.com>
2025-09-14 18:09:47 +0900

    webconfig 수정

--- src/main/java/com/team03/ticketmon/_global/config/Webconfig.java
+++ src/main/java/com/team03/ticketmon/_global/config/Webconfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
 
+public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
